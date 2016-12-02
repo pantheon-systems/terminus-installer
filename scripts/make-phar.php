bd804521dd9f035926841ac2a60103734d3635a4
tesladethray <tesladethray@gmail.com>
2016-12-02 11:00:56 -0800

    I hate you you're awful

---
+++ scripts/make-phar.php
@@ -0,0 +1,70 @@
+#!/usr/bin/env php
+<?php
+
+require './vendor/autoload.php';
+
+use Symfony\Component\Finder\Finder;
+
+if (!isset($argv[1])) {
+    echo "usage: php -dphar.readonly=0 $argv[0] <path> [--quiet]\n";
+    exit(1);
+}
+
+define('DEST_PATH', $argv[1]);
+
+define('BE_QUIET', in_array('--quiet', $argv));
+
+/**
+ * Adds a file to the PHAR
+ *
+ * @param Phar   $phar Phar archive resource
+ * @param string $path Path to the file to add
+ * @param array $options Options to alter the file being added
+ *      integer offset The number of lines to skip at the beginning of the file
+ */
+function addFile($phar, $path, $options = ['offset' => 0,])
+{
+    $key = str_replace('./', '', $path);
+    if (!BE_QUIET) {
+        echo "$key - $path\n";
+    }
+    $file_contents = file_get_contents($path);
+    if (isset($options['offset']) && (boolean)$options['offset']) {
+        $file_contents = implode("\n", array_slice(explode("\n", $file_contents), $options['offset']));
+    }
+    $phar[$key] = $file_contents;
+}
+
+$phar = new Phar(DEST_PATH, 0, 'installer.phar');
+
+$phar->startBuffering();
+
+addFile($phar, './bin/janus', ['offset' => 1,]);
+
+// PHP files
+$finder = new Finder();
+$finder
+    ->files()
+    ->ignoreVCS(true)
+    ->in('./src')
+    ->in('./vendor');
+
+foreach ($finder as $file) {
+    addFile($phar, $file);
+}
+
+addFile($phar, './vendor/autoload.php');
+addFile($phar, './vendor/rmccue/requests/library/Requests/Transport/cacert.pem');
+
+$phar->setStub(
+    <<<EOB
+#!/usr/bin/env php
+<?php
+Phar::mapPhar();
+include 'phar://installer.phar/bin/janus';
+__HALT_COMPILER();
+?>
+EOB
+);
+
+$phar->stopBuffering();
--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -2,6 +2,8 @@
 
 namespace Pantheon\Janus\Commands;
 
+use Symfony\Component\Yaml\Yaml;
+
 /**
  * Class InstallCommand
  * @package Pantheon\Janus\Commands
@@ -9,18 +11,110 @@ namespace Pantheon\Janus\Commands;
 class InstallCommand
 {
     /**
-     * Test command for testing
+     * @var string
+     */
+    private $constant_prefix = 'TERMINUS_';
+
+    /**
+     * Installs Terminus
      *
      * @command install
      * @aliases install:terminus
      *
-     * @option string $message A message to print
+     * @option string $install-dir Directory to which to install Terminus; default is $HOME/.composer/vendor/
+     * @option string $cache-dir Directory to which to place Terminus' cache; default is $HOME/.terminus/cache
+     * @option string $time-zone Time zone to print out Terminus datetimes in; default is UTC
+     * @option string $date-format ; default is Y-m-d H:i:s
      */
-    public function install($options = ['message' => null,])
-    {
-        if (!is_null($message = $options['message'])) {
-            return "The message is '$message'.";
+    public function install(
+        $options = ['install-dir' => null, 'cache-dir' => null, 'time-zone' => null, 'date-format' => null,]
+    ) {
+        //Install Composer if it is not present
+        /**if (!is_executable('composer')) {
+            exec("curl -sS https://getcomposer.org/installer | php -- --");
+        }**/
+        echo "Installin', yo.\n";
+        if (isset($options['install-dir']) && !is_null($install_dir = $options['install-dir'])) {
+            $install_dir = str_replace('~', $this->getHomeDir(), $install_dir);
+            exec("cd $install_dir ; composer require pantheon-systems/terminus");
+        } else {
+            exec("composer global require pantheon-systems/terminus");
         }
-        return "There is no message.";
+        $this->writeTerminusRC($options);
+        echo "You got if fam.\n";
+    }
+
+    private function writeTerminusRC($options)
+    {
+        $home_dir = $this->getHomeDir();
+        $config_file = "$home_dir/.terminusrc";
+        if (file_exists($config_file)) {
+            $contents = Yaml::parse(file_get_contents($config_file));
+            $file_config = array_combine(
+                array_map(
+                    function ($constant_name) {
+                        return $this->getKeyFromConstant($constant_name);
+                    },
+                    array_keys($contents)
+                ),
+                $contents
+            );
+            $options = array_merge($file_config, $options);
+        }
+
+        $file_text = '';
+        foreach ($options as $key => $value) {
+            if (!empty($value)) {
+                $file_text .= $this->getConstantFromKey($key) . ': ' . str_replace('~', $home_dir, $value) . PHP_EOL;
+            }
+        }
+
+        file_put_contents($config_file, $file_text);
+    }
+
+    /**
+     * Reflects a constant name from a given key
+     *
+     * @param string $key_name The name of a key to get a constant for
+     * @return string
+     */
+    private function getConstantFromKey($key_name)
+    {
+        return strtoupper(str_replace(['', '-'], [$this->constant_prefix, '_'], $key_name));
+    }
+
+    /**
+     * Returns the appropriate home directory.
+     *
+     * Adapted from Terminus Package Manager by Ed Reel
+     * @author Ed Reel <@uberhacker>
+     * @url    https://github.com/uberhacker/tpm
+     *
+     * @return string
+     */
+    private function getHomeDir()
+    {
+        $home = getenv('HOME');
+        if (!$home) {
+            $system = '';
+            if (getenv('MSYSTEM') !== null) {
+                $system = strtoupper(substr(getenv('MSYSTEM'), 0, 4));
+            }
+            if ($system != 'MING') {
+                $home = getenv('HOMEPATH');
+            }
+        }
+        return $home;
+    }
+
+    /**
+     * Reflects a key name given a Terminus constant name
+     *
+     * @param string $constant_name The name of a constant to get a key for
+     * @return string
+     */
+    private function getKeyFromConstant($constant_name)
+    {
+        return strtolower(str_replace([$this->constant_prefix, '_'], ['', '-'], $constant_name));
     }
 }
