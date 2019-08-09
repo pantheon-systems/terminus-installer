a8726eb59bc21804136a033d41a4b49a092b243b
tesladethray <tesladethray+github@gmail.com>
2019-08-09 16:17:40 -0700

    Checking for major version diffs to improve update

--- src/Command/UpdateCommand.php
+++ src/Command/UpdateCommand.php
@@ -5,6 +5,7 @@ namespace Pantheon\TerminusInstaller\Command;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
@@ -31,8 +32,108 @@ class UpdateCommand extends AbstractCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $this->output = $output;
-        $status_code = $this->updateTerminus($this->getDir($input->getOption('dir')));
+        $outdated_package_info = $this->standardizeOutdatedPackageInfo(
+            $this->getOutdatedPackageInfo($this->getDir($input->getOption('dir')))
+        );
+        $outdated_terminus_info = $this->getOutdatedTerminusInfo($outdated_package_info);
+        if (!empty((array)$outdated_terminus_info)) {
+            $this->validateTerminusData($outdated_terminus_info);
+            if ($this->isTerminusOutdatedByMajorVersion($outdated_terminus_info)) {
+                die('outdated by major version');
+                //install not update
+            } else {
+                die('is not so outdated 1');
+                $status_code = $this->updateTerminus($this->getDir($input->getOption('dir')));
+            }
+        } else {
+            die('is not so outdated 2');
+            $status_code = $this->updateTerminus($this->getDir($input->getOption('dir')));
+        }
         return $status_code;
+    }
+
+    /**
+     * Gets information for outdated packages
+     *
+     * @param string $update_dir Directory to which to update Terminus
+     * @return object $packageInfo Data returned by composer about outdated packages
+     */
+    private function getOutdatedPackageInfo($update_dir)
+    {
+        $arguments = [
+            'command' => 'outdated',
+            '--working-dir' => $update_dir,
+            '--format' => 'json',
+        ];
+
+        $this->output->writeln('Checking Terminus for updates...');
+        $outdatedOutput = new BufferedOutput();
+        $this->getComposer()->run(new ArrayInput($arguments), $outdatedOutput);
+        $packageInfo = json_decode($outdatedOutput->fetch());
+        return $packageInfo;
+    }
+
+    /**
+     * Standardizes the possible information coming back from the Composer Outdated command
+     *
+     * @param object|null $packageInfo Data in JSON format returned from composer outdated
+     * @return array The list of outdated installed packages
+     */
+    private function standardizeOutdatedPackageInfo($packageInfo) {
+        if (($packageInfo === null) || !isset($packageInfo->installed)) {
+            return [];
+        }
+        return $packageInfo->installed;
+    }
+
+    /**
+     * Ensures the vital properties for checking Terminus versions are present
+     *
+     * @param object $terminus_info Should have a latest and a version property
+     * @throws \Exception Is thrown if either of those properties are missing
+     */
+    private function validateTerminusData($terminus_info) {
+        if (!property_exists($terminus_info, 'latest')) {
+            throw new \Exception(
+                'Data returned from composer outdated did not include a latest version.'
+            );
+        }
+        if (!property_exists($terminus_info, 'version')) {
+            throw new \Exception(
+                'Data returned from composer outdated did not include a currently installed version.'
+            );
+        }
+    }
+
+    /**
+     * Checks for Terminus being outdated
+     *
+     */
+    private function isTerminusOutdatedByMajorVersion($terminus_info) {
+        if (!empty($terminus_info)) {
+            $latest_version = explode('.', $terminus_info->latest);
+            $latest_major_version = array_shift($latest_version);
+
+            $installed_version = explode('.', $terminus_info->version);
+            $latest_installed_version = array_shift($installed_version);
+
+            return $latest_installed_version < $latest_major_version;
+        }
+        return false;
+    }
+
+    /**
+     * @param object $package_info Stores outdated packages in the 'installed' property possibly DNE
+     * @return array
+     */
+    private function getOutdatedTerminusInfo($package_info) {
+        $filtered_info = array_filter($package_info, function($package) {
+            return $package->name === self::PACKAGE_NAME;
+        });
+        if (empty($filtered_info)) {
+            return (object)[];
+        }
+        return array_pop($filtered_info);
     }
 
     /**
@@ -41,7 +142,7 @@ class UpdateCommand extends AbstractCommand
      * @param string $update_dir Directory to which to update Terminus
      * @return integer $status_code The status code of the update operation run
      */
-    protected function updateTerminus($update_dir) {
+    private function updateTerminus($update_dir) {
         $arguments = [
             'command' => 'update',
             'packages' => [$this->getPackageTitle(),],
