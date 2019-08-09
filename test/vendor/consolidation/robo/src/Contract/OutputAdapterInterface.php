88f8262b949326793a0789e44ca3cd1840abbf9a
tesladethray <tesladethray+github@gmail.com>
2019-08-09 13:57:13 -0700

    this is how it was

---
+++ test/vendor/consolidation/robo/src/Contract/OutputAdapterInterface.php
@@ -0,0 +1,20 @@
+<?php
+namespace Robo\Contract;
+
+/**
+ * Adapt OutputInterface or other output function to the VerbosityThresholdInterface.
+ */
+interface OutputAdapterInterface
+{
+    /**
+     * @param int $verbosityThreshold
+     *
+     * @return bool
+     */
+    public function verbosityMeetsThreshold($verbosityThreshold);
+
+    /**
+     * @param string $message
+     */
+    public function writeMessage($message);
+}
---
+++ test/vendor/consolidation/robo/src/State/Consumer.php
@@ -0,0 +1,12 @@
+<?php
+namespace Robo\State;
+
+use Robo\State\Data;
+
+interface Consumer
+{
+    /**
+     * @return \Robo\State\Data
+     */
+    public function receiveState(Data $state);
+}
